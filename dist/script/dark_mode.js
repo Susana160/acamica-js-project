1d263b5c7fac533a304d735910fa7d9d64bc9a94
Susana160 <susa.016@hotmail.com>
2020-11-19 09:18:01 -0500

    desarrollo de modo dark con estilos y JS

--- dist/script/dark_mode.js
+++ dist/script/dark_mode.js
@@ -1,9 +1,8 @@
 let darkMode = document.getElementById("body_dark_mode");
 
 darkMode.addEventListener("click", function(event){
-    darkMode.classList.add("dark_mode");
+    darkMode.classList.toggle("dark_mode");
 });
+ 
 
-darkMode.addEventListener("dblclick", function(){
-    darkMode.classList.remove("dark_mode");
-});
+
